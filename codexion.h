a94638c97e6f70d8d9e3639d9cc48112c47e9a56
Jean-Yves Sam <jsam@z4r2p7.42lyon.fr>
2026-04-06 06:11:12 +0200

    test

--- codexion.h
+++ codexion.h
@@ -6,7 +6,7 @@
 /*   By: jsam <jsam@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/29 10:41:39 by jsam              #+#    #+#             */
-/*   Updated: 2026/03/29 10:41:42 by jsam             ###   ########lyon.fr   */
+/*   Updated: 2026/04/06 01:02:29 by jsam             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,31 @@
 
 # include <pthread.h>
 
+typedef struct s_data
+{
+	int	n_coders;
+	int	t_burnout;
+	int	t_compile;
+	int	t_debug;
+	int	t_refactor;
+	int	n_compiles;
+	int	t_cooldown;
+	char	*scheduler;
+}	t_data;
 
+typedef struct s_coder
+{
+	int	id;
+	pthread_t	thread;
+	int	last_compile_start;
+
+}	t_coder;
+
+typedef struct s_dongle
+{
+	int	id;
+	pthread_mutex mutex;
+	t_coder	coder_left;
+	t_coder	coder_right;
+}	t_dongle;
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jsam <jsam@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/29 10:20:52 by jsam              #+#    #+#             */
-/*   Updated: 2026/03/31 15:29:15 by jsam             ###   ########lyon.fr   */
+/*   Updated: 2026/04/06 00:30:15 by jsam             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <pthread.h>
 #include "codexion.h"
 #include <unistd.h>
+#include <string.h>
 
 #define N_THREAD 5
 #define INCREMENTS 1000
@@ -33,16 +34,120 @@ void *increment(void *arg)
 	return NULL;
 }
 
-int	main()
+void	cycle_coder(int n_compiles, t_coder *coder, t_dongle *dongle_1, t_dongle *dongle_2)
+{
+	int	i;
+
+	i = 0;
+	while (i < n_compiles)
+	{
+
+		i++;
+	}
+}
+
+int check_arg_int(int time, int min)
+{
+	if (time < min || time > 2147483647)
+		return -1;
+	return 0;
+}
+
+int check_int(char *arg)
+{
+	int i;
+	
+	i = 0;
+	if (!arg)
+		return -1;	
+	while (arg[i])
+	{
+		if (arg[i] < 30 || arg[i] > 39)
+			return -1;
+		i++;
+	}
+	return 0;
+}
+
+
+
+int	parser(int argc, char **argv)
+{
+	int i;
+	int *converted;
+	int result;
+
+	result = 0;
+	converted = malloc(sizeof(int) * 7);
+	if (!converted)
+		return -1;
+	if (argc != 9)
+		return -1;
+	i = 1;
+	while(i < 8 && !result)
+	{
+		if (!check_int(argv[i]))
+			return -1;
+		converted[i - 1] = atoi(argv[i]);	
+		if (!converted[i - 1])
+			result = -1;
+		i++;
+	}
+	if (!result && check_arg_int(converted[0], 2) < 0)
+		result = -1;
+	i = 1;
+	while (!result && !check_arg_int(converted[i], 1) && i < 7)
+		i++;
+	free(converted);
+	if (!result && i != 7)
+		result = -1;
+	if (!result && strcmp(argv[8], "fifo") != 0 && strcmp(argv[8], "edf") != 0)
+		result = -1;
+	return result;
+}
+
+t_data	*store_data(char **argv)
+{
+	t_data	*data;
+
+	if (!*argv)
+		return (NULL);
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (NULL);
+	data->n_coders = atoi(argv[1]);
+	data->t_burnout = atoi(argv[2]);
+	data->t_compile = atoi(argv[3]);
+	data->t_debug = atoi(argv[4]);
+	data->t_refactor = atoi(argv[5]);
+	data->n_compiles = atoi(argv[6]);
+	data->t_cooldown = atoi(argv[7]);
+	data->scheduler = argv[8];
+	return (data);
+}
+
+int	main(int argc, char **argv)
 {
 	pthread_t *threads;
+	t_data	*data;
+	pthread_mutex	global;
 	int i;
 
 	i = 0;
+
+	if (parser(argc, argv) < 0)
+	{
+		printf("ERROR");
+		return (1);
+	}
+	data = store(**argv);
+	if (!data)
+		return (1);
 	threads = malloc(sizeof(pthread_t) * N_THREAD);
 	if (!threads)
 		return 1;
 
+	
 	pthread_mutex_init(&counter_mutex, NULL);
 
 	while (i < N_THREAD)
